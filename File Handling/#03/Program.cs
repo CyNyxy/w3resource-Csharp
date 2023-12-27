0778cb588ea29cbffc3309d20d5af31c4d9e62d0
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-27 05:12:00 +0100

    File handling #3, #4 added

---
+++ File Handling/#03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //3. Write a C# Sharp program to create a blank file on the disk if the same file already exists.
+
+            string Fichero = @"C:\Users\Nyxy\Desktop\mytest.txt";
+
+            // FileStream F1 = new FileStream(Fichero, FileMode.Create);
+
+            if (File.Exists(Fichero))
+            {
+                File.Delete(Fichero);
+            }
+            Console.Write("\n\n Create a file in the disk if it is exists:\n");
+            Console.Write("-----------------------------------------------\n");
+
+            using (FileStream fileStr = File.Create(Fichero))
+            {
+                Console.WriteLine(" A file created with name mytest.txt\n\n");
+            }
+        }
+    }
+}
---
+++ File Handling/#04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace _04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4. Write a program in C# to create a file and add some text.
+
+            string Fichero = @"C:\Users\Nyxy\Desktop\mytest.txt";
+
+            FileStream F1 = new FileStream(Fichero, FileMode.Create, FileAccess.Write);
+
+            StreamWriter texto = new StreamWriter(F1);
+            texto.WriteLine("oh!");
+            texto.Close();
+            F1.Close();
+
+            //using (StreamWriter fileStr = File.CreateText(Fichero))
+            //{
+            //    fileStr.WriteLine("Hello");
+            //}
+        }
+    }
+}
