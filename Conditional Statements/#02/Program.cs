5e02876ebee1810a859547445d98bb990c294115
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-31 18:42:40 +0100

    #2 added

---
+++ Conditional Statements/#02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, rem;
+
+            Console.Write("Input number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            rem = num % 2;
+
+            if (rem == 0)
+            {
+                Console.WriteLine("{0} is an even integer", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is an odd integer", num);
+            }
+        }
+    }
+}
