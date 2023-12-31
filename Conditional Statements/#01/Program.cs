8159731cb8c7f88c0a745779728723ac3500efda
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-31 18:32:48 +0100

    Conditional statements #01 added

---
+++ Conditional Statements/#01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st number: ");
+            int a = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Input 2nd number: ");
+            int b = Int32.Parse(Console.ReadLine());
+                    //Convert.ToInt32(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.Write("{0} and {1} are equal", a,b);
+            }
+            else
+            {
+                Console.Write("{0} and {1} are not equal", a,b);
+            }
+        }
+    }
+}
