5b9a657acd875e79593618e2e1a3581fdd198c2c
CyNyxy <94080207+CyNyxy@users.noreply.github.com>
2023-12-31 19:16:18 +0100

    #04 added

---
+++ Conditional Statements/#04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //4. Write a C# program to find out whether a given year is a leap year or not.
+
+            int year;
+            Console.Write("Input a year: ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            //if (year % 4 == 0 && (!(year % 100 == 0) || (year % 400 == 0))) 
+            {
+                Console.WriteLine("{0} is a leap year", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap year", year);
+            }
+        }
+    }
+}
